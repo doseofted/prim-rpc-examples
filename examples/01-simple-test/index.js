97a7b7b2b7283cde36eb951fa7e3508b885af468
Ted Klingenberg <ted@doseofted.com>
2023-01-07 14:47:02 -0500

    Added some line breaks to example so it's easier to read

--- examples/01-simple-test/index.js
+++ examples/01-simple-test/index.js
@@ -6,6 +6,7 @@ import {
 } from "@doseofted/prim-rpc"
 
 // SECTION: start server code
+
 /**
  * Define any kind of function to be used with Prim+RPC.
  * This function is used from the server and will just say hello.
@@ -17,29 +18,38 @@ import {
 function sayHello(x, y) {
 	return `${x}, meet ${y}.`
 }
+
 // We'll assign an "rpc" property to the function so Prim+RPC knows it's safe to call this function
 sayHello.rpc = true
+
 // Let's call our function so we know what to expect.
 const expected = sayHello("Backend", "Frontend")
 // expected === "Backend, meet Frontend."
+
 // We'll create a module that will hold this function to be used directly with Prim+RPC
 // (this could be imported from somewhere else but we'll use the function we just defined)
 const module = { sayHello }
+
 // Let's create Prim+RPC plugins so the server and client can communicate with each other
 const plugins = testing.createPrimTestingPlugins()
+
 // We'll extract the server-side plugins to be used with Prim+RPC
 const { callbackHandler, methodHandler } = plugins
+
 // Now we pass the module and plugins to Prim+RPC server
 createPrimServer({
 	module,
 	callbackHandler,
 	methodHandler,
 })
+
 // !SECTION: end server code
 
 // SECTION: start client code
+
 // Next up is the client. Let's extract the client-side plugins.
 const { callbackPlugin, methodPlugin } = plugins
+
 // Now we'll pass those plugins to the Prim+RPC client
 // Given "@type" comment is optional and just helps with code-editor suggestions
 /** @type {import("@doseofted/prim-rpc").PromisifiedModule<typeof module>} */
@@ -47,8 +57,11 @@ const backend = createPrimClient({
 	callbackPlugin,
 	methodPlugin,
 })
+
 // We're done! Let's call our function from the client
 const greeting = await backend.sayHello("Backend", "Frontend")
+
 // Now we can log output and compare with expected result from the server
 console.log(greeting, greeting === expected)
+
 // !SECTION: end client code
--- examples/02-client-server/client.tsx
+++ examples/02-client-server/client.tsx
@@ -1,23 +1,28 @@
 /* @refresh reload */
 // Import the Prim+RPC client
 import { createPrimClient } from "@doseofted/prim-rpc"
+
 // Also, import the browser-specific plugins to communicate with our server
 // The "method" plugin uses the Fetch API and the "callback" plugin uses the WebSocket API
 import {
 	createMethodPlugin,
 	createCallbackPlugin,
 } from "@doseofted/prim-rpc-plugins/browser-api"
+
 // Import the module type from the server (use `import type` to import types only)
 import type { module } from "./server"
+
 // Import Solid dependencies (this isn't Prim+RPC related, just for displaying result)
 import { createResource } from "solid-js"
 import { render } from "solid-js/web"
+
 // Setup the Prim+RPC client and give it the endpoint of the configured server
 const backend = createPrimClient<typeof module>({
 	endpoint: "http://[::1]:3001/",
 	methodPlugin: createMethodPlugin(),
 	callbackPlugin: createCallbackPlugin(),
 })
+
 // App is the root component used by Solid
 function App() {
 	// Make a function call using Prim+RPC (`createResource` is Solid's way of handling returned promise)
@@ -27,5 +32,6 @@ function App() {
 	// Check if promise has resolved (loading === false) and then display returned value
 	return <p>{!greeting.loading && greeting()}</p>
 }
+
 // Render Solid app to HTML given in #root of index.html
 render(() => <App />, document.getElementById("root") as HTMLElement)
