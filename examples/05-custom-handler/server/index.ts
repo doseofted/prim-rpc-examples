c4b3656c1530ebb4e285ed3cc317dc64c8495657
Ted Klingenberg <ted@doseofted.com>
2023-01-24 02:07:17 -0500

    Swapped out Fastify for Express in custom-handler example (to demonstrate another server as well)

--- examples/05-custom-handler/server/index.ts
+++ examples/05-custom-handler/server/index.ts
@@ -3,16 +3,17 @@ import { createPrimServer, JsonHandler } from "@doseofted/prim-rpc"
 // Now import your JavaScript code to be used with Prim+RPC
 import * as module from "./hello"
 
-// And import plugins for intended servers (we'll be using `fastify` and `ws`)
-import { createMethodHandler } from "@doseofted/prim-rpc-plugins/fastify"
+// And import plugins for intended servers (we'll be using `express` and `ws`)
+import { createMethodHandler } from "@doseofted/prim-rpc-plugins/express"
 import { createCallbackHandler } from "@doseofted/prim-rpc-plugins/ws"
 
 // import server frameworks themselves
-import Fastify from "fastify"
+import { createServer } from "node:http"
+import express from "express"
 import { WebSocketServer } from "ws"
 
-// Import Fastify's `cors` module so that client can communicate with server
-import cors from "@fastify/cors"
+// Import Express's `cors` module so that client can communicate with server
+import cors from "cors"
 
 // import custom JSON handler
 import yaml from "yaml"
@@ -23,39 +24,26 @@ const jsonHandler: JsonHandler = {
 	mediaType: "application/yaml",
 }
 
-// Setup Fastify and WS Server (this isn't Prim+RPC related)
-const fastify = Fastify()
-const wss = new WebSocketServer({ server: fastify.server })
+// Setup Express and WS Server (this isn't Prim+RPC related)
+const app = express()
+const server = createServer(app)
+const wss = new WebSocketServer({ server })
 
-fastify.addContentTypeParser("application/yaml", (_req, payload, done) => {
-	let data: Buffer
-	payload.on("data", (chunk) => {
-		data = Buffer.concat([data, chunk].filter((given) => given))
-	})
-	payload.on("error", () => {
-		done(new Error("Buffer could not be read"))
-	})
-	payload.on("end", () => {
-		done(null, data.toString("utf-8"))
-	})
-})
-
-// Register `cors` module with fastify so that client can communicate with server
-await fastify.register(cors)
+// Register `cors` module with Express so that client can communicate with server
+app.use(cors())
 
 // Now create the Prim+RPC with the configured module and plugins for our servers
 createPrimServer({
 	prefix: "/",
 	module,
 	jsonHandler,
-	handleBlobs: false,
-	methodHandler: createMethodHandler({ fastify }),
+	methodHandler: createMethodHandler({ app }),
 	callbackHandler: createCallbackHandler({ wss }),
 })
 
-// Start listening with Fastify server (where Prim+RPC will be served)
-const address = await fastify.listen({ port: 3001 })
-console.log("Serving", address)
+// Start listening with Express server (where Prim+RPC will be served)
+server.listen(3001)
+console.log("Serving", server.address())
 
 // As a last step, export the type of our module to be used from the client
 // With "export type", only the type definitions are exported (no code is exported)
