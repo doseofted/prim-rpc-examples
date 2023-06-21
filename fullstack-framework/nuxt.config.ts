7af3c100e4ec28e0104288443692e51b25d8843e
Ted Klingenberg <ted@doseofted.com>
2023-06-21 17:57:59 -0400

    Removed preventImport plugin in fullstack example (since import is needed on server-side)

--- fullstack-framework/nuxt.config.ts
+++ fullstack-framework/nuxt.config.ts
@@ -1,11 +1,1 @@
-import path from "node:path"
-// The following is an (optional) safeguard to prevent server-side functions from being imported on the client
-import preventImport from "@doseofted/prim-rpc-tooling/build"
-
-export default defineNuxtConfig({
-	vite: {
-		// NOTE: This will keep accidental imports out of client bundle (Nitro bundle is unaffected)
-		// TODO: ^^^ make sure that Nitro is unaffected
-		// plugins: [preventImport.vite({ name: path.join(__dirname, "hello") })],
-	},
-})
+export default defineNuxtConfig({})
