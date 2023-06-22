b213f52346114c16c7108128efa26822720724e9
Ted Klingenberg <ted@doseofted.com>
2023-06-22 02:01:45 -0400

    Fullstack example is a little shorter

--- fullstack-framework/composables/backend.ts
+++ fullstack-framework/composables/backend.ts
@@ -2,11 +2,10 @@
 import { createPrimClient } from "@doseofted/prim-rpc"
 // Also, import the browser-specific plugins to communicate with our server
 import { createMethodPlugin } from "@doseofted/prim-rpc-plugins/browser"
-// Import the module type from the server (use `import type` to import types only)
-import type * as moduleType from "@/hello"
 
 // Setup the Prim+RPC client and give it the endpoint of the configured server
-export const backend = createPrimClient<typeof moduleType>({
+// Also provide module type to generic parameter (since module type is possibly is null)
+export const backend = createPrimClient<typeof import("@/hello")>({
 	module: process.server ? import("@/hello") : null,
 	endpoint: "/api/prim",
 	methodPlugin: createMethodPlugin(),
--- fullstack-framework/server/api/[...].ts
+++ fullstack-framework/server/api/[...].ts
@@ -2,16 +2,13 @@
 import { createPrimServer } from "@doseofted/prim-rpc"
 // And import plugins for intended server (we'll be using `h3` which powers Nuxt's Nitro server)
 import { defineH3PrimHandler } from "@doseofted/prim-rpc-plugins/h3"
-// Now import your JavaScript code to be used with Prim+RPC
-import * as myModule from "@/hello"
 
 // Now create the Prim+RPC with the configured module and plugin for our server
 const prim = createPrimServer({
+	module: import("@/hello"),
 	prefix: "/api/prim",
-	module: myModule,
 })
 
 // Unlike previous examples, we don't need to give the Prim+RPC server a method handler.
-// Instead we will create an H3 event handler and pass it an instance of our Prim+RPC server.
-// We need to do this here because Nuxt expects a default export containing an H3 route handler.
+// Instead we will export an H3 event handler and pass it an instance of our Prim+RPC server.
 export default defineH3PrimHandler({ prim })
