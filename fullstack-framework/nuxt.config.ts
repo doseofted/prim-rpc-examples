f559cc5e476fa68b673d5dc71206d896152044d6
Ted Klingenberg <ted@doseofted.com>
2023-06-26 15:34:08 -0400

    Added back prevent import tool to Nuxt example

--- fullstack-framework/nuxt.config.ts
+++ fullstack-framework/nuxt.config.ts
@@ -1,1 +1,16 @@
-export default defineNuxtConfig({})
+import path from "node:path"
+// The following is an (optional) safeguard to show an error if server-side functions are imported on the client
+import preventImport, {
+	BuildModifyMethod,
+} from "@doseofted/prim-rpc-tooling/build"
+
+export default defineNuxtConfig({
+	vite: {
+		plugins: [
+			preventImport.vite({
+				name: path.join(__dirname, "hello"),
+				method: BuildModifyMethod.RunTimeProcessCheck,
+			}),
+		],
+	},
+})
