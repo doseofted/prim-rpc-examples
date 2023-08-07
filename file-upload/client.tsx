1c2e6f20c27773f6dccbf9f6670b0866c56eb0c0
Ted Klingenberg <ted@doseofted.com>
2023-08-06 22:02:13 -0400

    Added skeleton of project to show off file uploads

---
+++ file-upload/client.tsx
@@ -0,0 +1,47 @@
+// Import the Prim+RPC client
+import { createPrimClient } from "@doseofted/prim-rpc"
+
+// Also, import the browser-specific plugins to communicate with our server
+// The "method" plugin uses the Fetch API and the "callback" plugin uses the WebSocket API
+import {
+	createMethodPlugin,
+	createCallbackPlugin,
+} from "@doseofted/prim-rpc-plugins/browser"
+
+// Import the module type from the server (use `import type` to import types only)
+import type { module } from "./server"
+
+// Import Solid dependencies (this isn't Prim+RPC related, just for displaying result)
+import { createResource } from "solid-js"
+import { render } from "solid-js/web"
+
+// Setup the Prim+RPC client and give it the endpoint of the configured server
+const backend = createPrimClient<typeof module>({
+	endpoint: "http://website.localhost:3001/",
+	methodPlugin: createMethodPlugin(),
+	callbackPlugin: createCallbackPlugin(),
+})
+
+// App is the root component used by Solid. The return value will become HTML.
+function App() {
+	// Make a function call using Prim+RPC (`createResource` is Solid's way of handling returned promise)
+	const [greeting] = createResource(async () => {
+		const greeting = await backend.sayHello("Backend", "Frontend")
+		console.log(greeting) // we can log result to see the result is just a string
+		return greeting
+	})
+	// Check if promise has resolved (loading === false) and then display returned value
+	return <p>{!greeting.loading && greeting()}</p>
+}
+
+// Render Solid app to HTML given in #root of index.html
+const root = document.getElementById("root") as HTMLElement
+const dispose = render(() => <App />, root)
+
+// ignore this bit (it's just in case server isn't running yet in development mode)
+if (import.meta.hot) {
+	import.meta.hot?.dispose(dispose)
+	fetch("http://website.localhost:3001/health").catch(() =>
+		setTimeout(() => location.reload(), 300)
+	)
+}
---
+++ file-upload/server/index.ts
@@ -0,0 +1,44 @@
+// First, import Prim+RPC server
+import { createPrimServer } from "@doseofted/prim-rpc"
+// Now import your JavaScript code to be used with Prim+RPC
+import * as module from "./hello"
+
+// And import plugins for intended servers (we'll be using `fastify` and `ws`)
+import { createMethodHandler } from "@doseofted/prim-rpc-plugins/fastify"
+import { createCallbackHandler } from "@doseofted/prim-rpc-plugins/ws"
+
+// import server frameworks themselves
+import Fastify from "fastify"
+import { WebSocketServer } from "ws"
+
+// Import Fastify's `cors` module so that client can communicate with server
+import cors from "@fastify/cors"
+// import multipart plugin to add support for file uploads to Fastify
+import multipartPlugin from "@fastify/multipart"
+// add ability to easily serve uploaded files
+import fastifyStatic from "@fastify/static"
+
+// Setup Fastify and WS Server (this isn't Prim+RPC related)
+const fastify = Fastify()
+const wss = new WebSocketServer({ server: fastify.server })
+
+// Register `cors` module with fastify so that client can communicate with server
+await fastify.register(cors)
+await fastify.register(fastifyStatic, { root: "./uploads", prefix: "/uploads" })
+fastify.get("/health", () => "ok") // simple health check
+
+// Now create the Prim+RPC with the configured module and plugins for our servers
+createPrimServer({
+	prefix: "/",
+	module,
+	methodHandler: createMethodHandler({ fastify, multipartPlugin }),
+	callbackHandler: createCallbackHandler({ wss }),
+})
+
+// Start listening with Fastify server (where Prim+RPC will be served)
+const address = await fastify.listen({ port: 3001 })
+console.log("Serving", address)
+
+// As a last step, export the type of our module to be used from the client
+// With "export type", only the type definitions are exported (no code is exported)
+export type { module }
