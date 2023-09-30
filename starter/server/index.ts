9aebe161672847da8c517ad66fbc3627e6dc7b39
Ted Klingenberg <ted@doseofted.com>
2023-09-29 20:20:14 -0400

    Starter now auto-opens browser window, checks that separate server is running in browser

--- starter/server/index.ts
+++ starter/server/index.ts
@@ -3,6 +3,12 @@ import { createServerAdapter } from "@whatwg-node/server"
 
 // TODO: Create the Prim+RPC server here
 
-const fetchAdapter = createServerAdapter(() => new Response("Not implemented"))
+const cors = {
+	"access-control-allow-origin": "http://localhost:5173",
+	"access-control-allow-headers": "content-type",
+}
+const fetchAdapter = createServerAdapter(
+	() => new Response("Not implemented", { headers: cors })
+)
 createServer(fetchAdapter).listen(3000)
 console.log("Server is running at http://localhost:3000/")
