63b1c4ca1b298e2d56161fef68bcff62914bb7f4
Ted Klingenberg <ted@doseofted.com>
2023-10-15 02:04:06 -0400

    Fix for starter in stackblitz

--- starter/client/index.ts
+++ starter/client/index.ts
@@ -1,8 +1,7 @@
 // TODO: call your function using Prim+RPC here
 
 const app = document.getElementById("app")
-if (!app) throw new Error("No #app element found")
-app.innerText = "Not implemented (yet)"
+if (app) app.innerText = "Not implemented (yet)"
 
 // NOTE: ignore this bit: it's just here to make sure the server is running
 if (import.meta.env.DEV)
